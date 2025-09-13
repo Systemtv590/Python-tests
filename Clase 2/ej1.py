7c1692458022fe059fa845416d7b0d2bc2537c28
Systemtv590 <81577070+Systemtv590@users.noreply.github.com>
2025-09-13 13:43:01 -0300

    clase 2

---
+++ Clase 2/ej1.py
@@ -0,0 +1,11 @@
+num1 = input("Elige un numero")
+num2 = input("Elige otro numero")
+if num1 % 2 = 0
+    print("num1 es Par")
+else
+    print("num1 es Impar")
+
+if num2 % 2 = 0
+    print("num2 es Par")
+else
+    print("num1 es Impar")
---
+++ Clase 2/test.py
@@ -0,0 +1,9 @@
+print("holaaaa")
+
+entero = 10
+boleano = False
+flotante = 3.14
+array=["Uno", "Dos"]
+
+print("El nuimero es",entero)
+print("La variable es un", type(entero))
